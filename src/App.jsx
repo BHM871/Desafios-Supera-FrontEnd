18795e4a72a7652d06dfd10acd26dcda0f64433d
BHM871 <adrianalmeida417@gmail.com>
2023-07-14 01:42:19 -0300

    Adicionando estilo  diretório principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Form from "./components/form/Form";
 import Table from "./components/table/Table";
+import './css/App.css';
 
 function App(){
     return (
         <>
-            <div>
-                <Form metodo={"GET"} />
-            </div>
-            <div>
-                <Table />
+            <div className="father-div">
+                <div className="children-div">
+                    <Form metodo={"GET"} />
+                </div>
+                <div className="children-div">
+                    <Table />
+                </div>
             </div>
         </>
     )
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -7,14 +7,14 @@ import '/src/css/Table.css';
 
 function Table() {
     return (
-        <div>
+        <>
             <table style={{width: '100%'}}>
                 <Head saldoTotal={50} saldoNoPeriodo={100}/>
                 <Topics />
                 <Content dataTransacao={"25/02/2019"} valorTransacao={504.23} tipoTransacao={"Deposito"} nomeTransacionado={"Fulano"}/>
                 <Footer />
             </table>
-        </div>
+        </>
     )
 }
 
