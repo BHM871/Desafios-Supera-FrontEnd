ee856c43d31e6e2307bf230800e1f11953c7ee0c
BHM871 <adrianalmeida417@gmail.com>
2023-07-14 01:04:44 -0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import Form from "./components/Form/Form";
-import Table from "./components/Table/Table";
 
 function App(){
-    return (
-        <>
-            <div>
-                <Form metodo={"GET"} />
-            </div>
-            <div>
-                <Table />
-            </div>
-        </>
-    )
+    return <h1>Ola mundo</h1>
 }
 
 export default App
